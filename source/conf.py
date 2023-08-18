157d4f3c9dac86a0039283d3a79b8bb2ed3a395d
GloomyShaw <142489011+GloomyShaw@users.noreply.github.com>
2023-08-18 17:04:40 +0800

    source

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Test1'
+copyright = '2023, GloomyShaw'
+author = 'GloomyShaw'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+locale_dirs = ['../locales/']
+gettext_compact = False 
+gettext_uuid = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_search_language = 'zh_CN'
+
+html_static_path = ['_static']
---
+++ source/poem.rst
@@ -0,0 +1,18 @@
+诗
+==
+
+trouble go away 
+
+**麻烦全走开**
+
+love always stay 
+
+**爱情永远在**
+
+happiness on the way 
+
+**幸福总会来**
+
+hey hey 
+
+**嗨嗨**
